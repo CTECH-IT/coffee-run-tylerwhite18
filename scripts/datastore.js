9d455a063b77d355b0ec726e6a36c8c61caedb57
Tyler White <7670501@isd535.org>
2022-03-09 11:33:13 -0600

    private var

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -2,25 +2,26 @@
     'use strict';
     let App = window.App || {}; /*either declares App as the window element,
     or, if no element exists, makes an empty object*/
+    let data;
 
     function DataStore() {
-        this.data = {};
+        data = {};
     }
 
     DataStore.prototype.add = function (key, val) {
-        this.data[key] = val;
+        data[key] = val;
     };
 
     DataStore.prototype.get = function (key) {
-        return this.data[key];
+        return data[key];
     };
 
     DataStore.prototype.getAll = function () {
-        return this.data;
+        return data;
     };
 
     DataStore.prototype.remove = function(key) {
-        delete this.data[key];
+        delete data[key];
     };
 
     App.DataStore = DataStore;

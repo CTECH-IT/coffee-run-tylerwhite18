7b0f9bd9c2adaec0948e8852f1800cf2fa59a5cd
Tyler White <7670501@isd535.org>
2022-03-15 11:25:55 -0500

    fixed syntax to be bett and added call to checklist constructor

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,23 @@
     'use strict';
 
     const FORM_SELECTOR = '[data-coffee-order="form"]';
+    const CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
 
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
     let FormHandler = App.FormHandler;
+    let CheckList = App.CheckList;
 
     let myTruck = new Truck('1', new DataStore());
+    let checkList = new CheckList(CHECKLIST_SELECTOR);
     window.myTruck = myTruck;
 
     let formHandler = new FormHandler(FORM_SELECTOR);
-    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+    formHandler.addSubmitHandler(function (data) {
+        myTruck.createOrder.call(myTruck, data);
+        checkList.addRow.call(checkList, data);
+    });
     console.log(formHandler);
 })(window);
 const SLIDER_LOC = 'strengthLevel';
@@ -24,23 +30,23 @@ let outBox = document.getElementById(SLIDE_OUT);
 let quoteBox = document.querySelector(MESSAGE);
 slider.addEventListener("input", function () {
     let val = slider.value
-    outBox.innerText = val;
+    outBox.innerHTML = val;
     if (val == 100) {
-        quoteBox.innerText = 'That should do it.';
+        quoteBox.innerHTML = 'That should do it.';
     }
     else if (val >= 80) {
-        quoteBox.innerText = 'You might almost be woken up.';
+        quoteBox.innerHTML = 'You might almost be woken up.';
     }
     else if (val >= 55) {
-        quoteBox.innerText = 'Keep going. We dont take weak coffee orders.';
+        quoteBox.innerHTML = 'Keep going. We dont take weak coffee orders.';
     }
     else if (val >= 30) {
-        quoteBox.innerText = 'Your coffee is going to need to hit the gym at this rate.';
+        quoteBox.innerHTML = 'Your coffee is going to need to hit the gym at this rate.';
     }
     else if (val >= 20) {
-        quoteBox.innerText = 'You moved the slider the wrong direction.';
+        quoteBox.innerHTML = 'You moved the slider the wrong direction.';
     }
     else {
-        quoteBox.innerText = 'Enjoy your coffee-flavored water.';
+        quoteBox.innerHTML = 'Enjoy your coffee-flavored water.';
     }
 });
